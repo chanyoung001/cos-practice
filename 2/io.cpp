4ae4e30707033bd961e1857e88cf0cd96bbd6bdf
Chanyoung Park <qkrcksdud0508@gmail.com>
2026-03-19 01:45:44 +0000

    practice 3

---
+++ 2/io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int x{}; //the integer variable
+	cout << "Enter a number: ";
+	cin >> x;
+	cout << "You entered " << x
+<<endl;
+	return 0;
+}
+
