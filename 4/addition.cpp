9edd041a87327644d38daafa42f6b99cf0b88c04
Chanyoung Park <qkrcksdud0508@gmail.com>
2026-03-29 16:32:59 +0000

    addition review

--- 4/addition.cpp
+++ 4/addition.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-int uadd ok(unsigned a, unsigned b) {}
-int tadd ok(int a, int b) {}
+int uadd_ok(unsigned a, unsigned b) {}
+int tadd_ok(int a, int b) {}
 int main(int argc, char *argv[])
 {
 	int a, b;
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 	x = (unsigned) a;
 	y = (unsigned) b;
 	cout << "Unsigned Addition ===" << endl;
-	if (uadd ok(x,y))
+	if (uadd_ok(x,y))
 		cout << "No overflow" << endl;
 	else
 		cout << "Overflow" << endl;
 
 	cout << "Signed Addition ===" << endl;
-	if (tadd ok(a,b))
+	if (tadd_ok(a,b))
 		cout << "No overflow" << endl;
 	else
 		cout << "Overflow" << endl;
